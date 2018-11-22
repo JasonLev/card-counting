923b1317103916524e42c9d834a61def6b49cade
JasonLev <jasond.levy@gmail.com>
2018-11-22 12:42:22 -0800

    f/setup with container and reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+// import { combineReducers } from 'redux'
+// import cards from './cards'
+// import visibilityDisplay from './visibleDisplay'
+
+// export default combineReducers({
+//   cards,
+//   visibilityDisplay
+// })
+export default (state = [], action) => {
+  return state
+}
