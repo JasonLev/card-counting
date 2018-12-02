db952f08e3be372e503dd6728f522fd01cc99641
JasonLev <jasond.levy@gmail.com>
2018-12-02 08:42:25 -0800

    f/styling for card-deck

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,24 @@
 import React from 'react'
 
 const Card = ({rank, suit}) => {
-  if (suit === "♣︎" || suit === "♠︎") {
-    return (
-      <div className="card card-black">
-        <div className="card-tl">
-          <div className="card-rank">{rank}</div>
-          <div className="card-suit">{suit}</div>
-        </div>
-        <div className="card-br">
-          <div className="card-rank">{rank}</div>
-          <div className="card-suit">{suit}</div>
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div className="card card-red">
-        <div className="card-tl">
-          <div className="card-rank">{rank}</div>
-          <div className="card-suit">{suit}</div>
-        </div>
-        <div className="card-br">
-          <div className="card-rank">{rank}</div>
-          <div className="card-suit">{suit}</div>
+  let cardColor = (suit === "♣︎" || suit === "♠︎") ? "card-black": "card-red";
+  return (
+    <div className="card-outline">
+      <div className={"card " + cardColor}>
+        <div className="front"></div>
+        <div className="back">
+          <div className="card-tl">
+            <div className="card-rank">{rank}</div>
+            <div className="card-suit">{suit}</div>
+          </div>
+          <div className="card-br">
+            <div className="card-rank">{rank}</div>
+            <div className="card-suit">{suit}</div>
+          </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Card
